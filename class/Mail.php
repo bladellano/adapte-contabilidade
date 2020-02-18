a455fbabc4634bb0d392dc4ee459e35aa7dee0c1
Caio Silva <bladellano@gmail.com>
2020-02-18 09:08:59 -0300

    Configurando envio de e-mail novamente

--- class/Mail.php
+++ class/Mail.php
@@ -6,13 +6,13 @@ use PHPMailer\PHPMailer\Exception;
 
 require("vendor/autoload.php");
 
-define('SMTP_USER', 'bladellano@gmail.com');
+define('SMTP_USER', 'contato@adaptcontabilidade.com.br');
 // define('SMTP_USER', 'contato@rosadesaroncolegio.com.br');
 // define('SMTP_USER', 'contato@adaptecontabilidade.com.br');
-define('SMTP_PASS', 'nt1@2012');
+define('SMTP_PASS', '#vO?Pm5{,9');
 // define('SMTP_PASS', 'nt1@2012');
 // define('SMTP_PASS', 'Admin2020!');
-define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_HOST', 'smtp.umbler.com');
 // define('SMTP_HOST', 'mail.paideiaeducacional.com.br');
 // define('SMTP_PORT', 465); 
 define('SMTP_PORT', 587); 
@@ -28,11 +28,11 @@ class Mail{
 		// $mail->SMTPSecure = 'ssl';    
 		// $mail->SMTPSecure = 'tls';    
 		$mail->SMTPDebug = SMTP::DEBUG_SERVER;   
-		// $mail->CharSet = 'UTF-8';		 
+		$mail->CharSet = 'UTF-8';		 
 		$mail->isHTML(true); 
 		$mail->SMTPAuth = true; // Autenticação		
 
-		
+
 		$mail->Host = SMTP_HOST;	 
 		$mail->Port = SMTP_PORT;  	 
 		$mail->Username = SMTP_USER;
